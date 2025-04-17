52213921b4bb5246d9899645a9130e8b477708c9
jerry <soaringjerry@gmail.com>
2025-04-18 04:16:52 +0800

    CI/CD Update

--- frontend-vue/src/config/index.js
+++ frontend-vue/src/config/index.js
@@ -1,5 +1,5 @@
 import yaml from 'js-yaml';
-import configData from './config.yaml'; // Temporarily remove ?raw for testing resolution
+import configData from './config.yaml?raw'; // 利用Vite原生功能以raw文本导入
 
 const config = yaml.load(configData);
 
